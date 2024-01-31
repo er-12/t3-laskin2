91aef22af8f9718e03510a336d2511171f0f87f1
er-12 <83115921+er-12@users.noreply.github.com>
2024-01-31 19:18:17 +0200

    Update App.js

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 150
+    marginTop: 150
   },
   input: {
     width: 200,
